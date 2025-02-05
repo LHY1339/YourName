abd20039b2f81e4a42fedee3927e16c030f929be
LHY1339 <123950909+LHY1339@users.noreply.github.com>
2025-02-05 17:00:18 +0800

    Initial commit

---
+++ Source/YourName/YourNameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "YourNameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class YOURNAME_API AYourNameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
